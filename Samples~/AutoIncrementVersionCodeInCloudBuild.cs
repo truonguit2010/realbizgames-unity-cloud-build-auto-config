3f05f0b837c6619e46adb61f4d2b2a8c2a87277f
DarkNight <truonguit2010@gmail.com>
2021-06-13 11:15:07 +0700

    - Add more document for maintain purpose.

---
+++ Samples~/AutoIncrementVersionCodeInCloudBuild.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AutoIncrementVersionCodeInCloudBuild : MonoBehaviour
+{
+    
+    #if UNITY_CLOUD_BUILD
+    public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
+    {
+#if AMAZON_STORE
+        UnityPurchasingEditor.TargetAndroidStore(AndroidStore.AmazonAppStore);
+#endif
+        string buildNumber = manifest.GetValue("buildNumber", "0");
+        VersionIncrementEdior.increaseBuildVersion(buildNumber);
+    }
+#endif
+
+}
